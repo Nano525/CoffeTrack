bbc8fd16f4292394c931b3e232c311f1baf704c6
Nano525 <20243ds020@utez.edu.mx>
2025-10-22 18:06:41 -0600

    Ventana de registro

--- src/main/java/mx/edu/utez/ads/Launcher.java
+++
@@ -1,9 +0,0 @@
-package mx.edu.utez.ads;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
