e55f7aaef80deac4a2c290280660c37afcef0f45
Nano525 <20243ds020@utez.edu.mx>
2025-10-25 16:42:14 -0600

    Archivo WindowUtils Creado para la navegacion de las pantallas, OracleDatabaseConnectionManager tambien ya esta creado para la conexion a la base de datos, modifique el module y el pom lo mas probable esque en un futuro tengamos que modificarlo

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module mx.edu.utez.ads {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires static ucp;
 
-
-    opens mx.edu.utez.ads to javafx.fxml;
-    exports mx.edu.utez.ads;
+    opens mx.edu.utez.ads.utils to javafx.fxml;
+    exports mx.edu.utez.ads.utils;
 }
